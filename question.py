dc1a08a81e75782957f6afe7c24e41aff49afdb8
NataliaMariaGentili <natigenti987@gmail.com>
2025-03-17 00:30:29 -0300

    Inciso A

--- question.py
+++ question.py
@@ -1,5 +1,6 @@
 import random
-# Preguntas para el juego
+import sys #importé sys para poder hacer un exit status
+
 questions = [
     "¿Qué función se usa para obtener la longitud de una cadena en Python?",
     "¿Cuál de las siguientes opciones es un número entero en Python?",
@@ -7,7 +8,7 @@ questions = [
     "¿Cuál de las siguientes expresiones es un comentario válido en Python?",
     "¿Cuál es el operador de comparación para verificar si dos valores son iguales?",
 ]
-# Respuestas posibles para cada pregunta, en el mismo orden que las preguntas
+
 answers = [
     ("size()", "len()", "length()", "count()"),
     ("3.14", "'42'", "10", "True"),
@@ -15,29 +16,38 @@ answers = [
     ("// Esto es un comentario","/* Esto es un comentario */","-- Esto es un comentario","# Esto es un comentario",),
     ("=", "==", "!=", "==="),
 ]
-# Índice de la respuesta correcta para cada pregunta, el mismo orden que las preguntas
+
 correct_answers_index = [1, 2, 0, 3, 1]
-# El usuario deberá contestar 3 preguntas
+
 for _ in range(3):
-    # Se selecciona una pregunta aleatoria
+    
     question_index = random.randint(0, len(questions) - 1)
-    # Se muestra la pregunta y las respuestas posibles
+    
     print(questions[question_index])
     for i, answer in enumerate(answers[question_index]):
-    print(f"{i + 1}. {answer}")
-    # El usuario tiene 2 intentos para responder correctamente
+        print(f"{i + 1}. {answer}")
+    
     for intento in range(2):
-        user_answer = int(input("Respuesta: ")) - 1
-        # Se verifica si la respuesta es correcta
-        if user_answer ==
-            correct_answers_index[question_index]:
+       
+        #ejecutamos la lectura input con un manejo de errores
+        try:
+            user_answer = int(input("Respuesta: ")) - 1
+        except ValueError:
+            print("Debes elegir un número")
+            sys.exit(1)
+        if user_answer == correct_answers_index[question_index]:
             print("¡Correcto!")
             break
+        #evaluamos que el numero este dentro del rango
+        elif (user_answer < 0) or (user_answer > (len(answers[question_index])-1)):
+            print("Respuesta no válida")
+            sys.exit(1)
+        #si esta dentro del rango, pero no es el numero se ejecuta el else
         else:
-            # Si el usuario no responde correctamente después de 2 intentos,
-            # se muestra la respuesta correcta
-            print("Incorrecto. La respuesta correcta es:")
-            print(answers[question_index]
+            print("Incorrecto. Intente de nuevo")
+    else:
+        print("Incorrecto. La respuesta correcta es:")
+        print(answers[question_index]
     [correct_answers_index[question_index]])
-    # Se imprime un blanco al final de la pregunta
+    
     print()
