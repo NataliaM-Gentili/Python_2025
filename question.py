df344cf2a68d8686aef1d2c289308a9349ab0679
NataliaMariaGentili <natigenti987@gmail.com>
2025-03-17 12:46:33 -0300

    Inciso C

--- question.py
+++ question.py
@@ -20,14 +20,17 @@ answers = [
 correct_answers_index = [1, 2, 0, 3, 1]
 puntaje = 0 #inicializamos los puntos
 
-for _ in range(3):
-    
-    question_index = random.randint(0, len(questions) - 1)
-    
-    print(questions[question_index])
-    for i, answer in enumerate(answers[question_index]):
+#agregamos la ayuda dada para el inciso C
+questions_to_ask = random.choices(list(zip(questions,answers, correct_answers_index)), k=3)
+#k = 3 indica que se tomaran tuplas, por lo que no es necesario utilizar for in range (3)
+#se "igualan" (posicionalmente hablando) las listas, por lo que la pregunta 0 se corresponde con las respuestas en la posicion 0 y la opcion correcta de la posicion 0   
+for pregunta, respuestas, correcta in questions_to_ask:
+    #al no haber indices, se imprime directamente la variable de la estructura for
+    print(pregunta) # print(questions[question_index])
+
+    for i, answer in enumerate(respuestas): #imprimimos las posibles respuestas
         print(f"{i + 1}. {answer}")
-    
+   
     for intento in range(2):
        
         #ejecutamos la lectura input con un manejo de errores
@@ -36,22 +39,23 @@ for _ in range(3):
         except ValueError:
             print("Debes elegir un número")
             sys.exit(1)
-        if user_answer == correct_answers_index[question_index]:
+
+        if user_answer == correcta: #correct_answers_index[question_index]
             print("¡Correcto!")
             puntaje += 1
             break
         #evaluamos que el numero este dentro del rango
-        elif (user_answer < 0) or (user_answer > (len(answers[question_index])-1)):
+        elif (user_answer < 0) or (user_answer > (len(respuestas)-1)): #(len(answers[question_index])-1))
             print("Respuesta no válida")
             sys.exit(1)
         #si esta dentro del rango, pero no es el numero se ejecuta el else
         else:
             print("Incorrecto. Intente de nuevo")
             puntaje = puntaje - 0.5
+
     else:
         print("Incorrecto. La respuesta correcta es:")
-        print(answers[question_index]
-    [correct_answers_index[question_index]])
+        print(f"{correcta+1}. {respuestas[correcta]}")
     print()
 print(f"Usted ha obtenido un total de ",puntaje," puntos") #imprimimos el puntaje total
     
